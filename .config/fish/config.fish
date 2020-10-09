84f8c117f218634f541bc6c2394adc8a1c0d2b37
TalkingPanda0 <memretwitch3626@gmail.com>
2020-10-09 12:51:12 +0000

    Fixed startx

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,7 @@
 set fish_greeting ""
 # Start X at login
-if [ (pgrep qtile | wc -l ) -lt 1 ]
-             exec startx -- -keeptty 
-   end
-
-
-
+if [ (ps ax | grep $fish_pid | awk '{print $2}' | grep tty1 | wc -l) -gt 1 ]
+	if [ (pgrep qtile | wc -l ) -lt 1 ]
+        	     exec startx -- -keeptty 
+	   end
+end
