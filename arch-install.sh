eb15da867436636a6cd872e8822f3e24ac11f438
TalkingPanda0 <memretwitch3626@gmail.com>
2021-01-17 17:58:09 +0300

    '

--- arch-install.sh
+++ arch-install.sh
@@ -16,11 +16,15 @@ then
     echo "yay installed"
 fi
 echo "yay Found"
+cp  -r wallpaper ~/
 cp  -r .bin ~/
 chmod +x ~/.bin/*
 cp -r .config/* ~/.config/
 cd ~/
-yay -S picom-jonaburg-git python3-pip qtile rofi picom termite betterlockscreen qtile nemo nitrogen xclip lxsession materia-gtk-theme qt5-styleplugins lxappearance flameshot pnmixer xfce4-volumed-pulse firefox redshift-gtk-git -y
+yay -S picom python3-pip qtile rofi picom termite betterlockscreen qtile nemo python-pywal xclip lxsession materia-gtk-theme qt5-styleplugins lxappearance flameshot pnmixer xfce4-volumed-pulse firefox redshift-gtk-git archlinux-wallpapers -yu
 git clone https://github.com/TalkingPanda0/dwm.git && cd dwm && sudo make install
 git clone https://github.com/TalkingPanda0/st.git && cd st && sudo make install
+git clone https://github.com/TalkingPanda0/slstatus.git && cd slstatus && sudo make install
+wal -i /usr/share/backgrounds/archlinux/awesome.png
+
 echo "done"
