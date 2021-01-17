2402db6c58be97ac8ffd1a650b1d4da3bb6d76df
TalkingPanda0 <memretwitch3626@gmail.com>
2021-01-17 18:31:26 +0300

    '

--- arch-install.sh
+++ arch-install.sh
@@ -19,7 +19,9 @@ echo "yay Found"
 cp  -r wallpaper ~/
 cp  -r .bin ~/
 chmod +x ~/.bin/*
+mkdir -p ~/.config
 cp -r .config/* ~/.config/
+cp -r .mozilla/ ~/
 cd ~/
 yay -S picom python3-pip qtile rofi picom termite betterlockscreen qtile nemo python-pywal xclip lxsession materia-gtk-theme qt5-styleplugins lxappearance flameshot pnmixer xfce4-volumed-pulse firefox redshift-gtk-git archlinux-wallpapers -yu
 git clone https://github.com/TalkingPanda0/dwm.git && cd dwm && sudo make install
