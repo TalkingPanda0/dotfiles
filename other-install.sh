51487e8404b910f8fa8a0c98618ffa0686005e8b
TalkingPanda0 <memretwitch3626@gmail.com>
2021-01-17 17:37:55 +0300

    '

--- other-install.sh
+++ other-install.sh
@@ -7,7 +7,5 @@ cp default.css ~/.surf/styles/
 cd ~/
 git clone https://github.com/TalkingPanda0/dwm.git && cd dwm && sudo make install
 git clone https://github.com/TalkingPanda0/st.git && cd st && sudo make install
-git clone https://github.com/TalkingPanda0/tabbed.git && cd tabbed && sudo make install
-git clone https://github.com/TalkingPanda0/surf.git && cd surf && sudo make install
 
 echo "done"
